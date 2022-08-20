e8493b9e03577ae36bdf3636d606ad488c730360
CherishMvp <bbttcherish@outlook.com>
2022-08-20 09:40:15 +0800

    review

--- test/project1/src/components/HelloWorld.vue
+++ test/project1/src/components/HelloWorld.vue
@@ -1,38 +1,46 @@
-<script setup lang="ts">
-import { ref } from 'vue'
-
-defineProps<{ msg: string }>()
-
-const count = ref(0)
-</script>
-
 <template>
-  <h1>{{ msg }}</h1>
-
-  <div class="card">
-    <button type="button" @click="count++">count is {{ count }}</button>
-    <p>
-      Edit
-      <code>components/HelloWorld.vue</code> to test HMR
-    </p>
+  <div>
+    <p>name from state:{{ firstStore.name }}</p>
+    <p>use storeToRefs:{{ name }}——{{ age }}</p>
+    {{ count }}
+    <h2>Button</h2>
+    <button type="primary" @click="a1">普通方式修改name</button><br />
+    <button type="primary" @click="a2">实例的对象形式修改多个state参数</button
+    ><br />
+    <button type="primary" @click="a3">实例的函数形式修改多个state参数</button>
   </div>
-
-  <p>
-    Check out
-    <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank"
-      >create-vue</a
-    >, the official Vue + Vite starter
-  </p>
-  <p>
-    Install
-    <a href="https://github.com/johnsoncodehk/volar" target="_blank">Volar</a>
-    in your IDE for a better DX
-  </p>
-  <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
 </template>
 
+<script setup lang="ts">
+import { useFirstStore } from "../store/index";
+import { storeToRefs } from "pinia";
+
+const firstStore = useFirstStore();
+// 解构赋值，直接将读取到的变量变成响应式数据
+const { name, age, count } = storeToRefs(firstStore);
+console.log("收到：", firstStore);
+
+// 修改state数据的几种办法
+// 1.
+const a1 = () => {
+  name.value = "cherish";
+};
+// 2.通过简单的对象的形式
+const a2 = () => {
+  firstStore.$patch({
+    name: "zgl",
+    age: 20,
+  });
+};
+
+// 3.通过函数的形式，通过实例对象的$patch来修改state的属性
+const a3 = () => {
+  firstStore.changeMethod3();
+};
+</script>
+
 <style scoped>
-.read-the-docs {
-  color: #888;
+p {
+  color: #b3a;
 }
 </style>
--- test/project1/src/store/index.ts
+++ test/project1/src/store/index.ts
@@ -1,9 +1,32 @@
 import { defineStore } from "pinia";
 
 export const useFirstStore = defineStore("first", {
-  actions: {},
-  getters: {},
+  // actions逻辑处理，可以同步也可以异步函数，same as methods
+  actions: {
+    // 将方式3移到acyions中处理
+    changeState(age: number) {
+      this.$patch((state: any) => {
+        state.age += age;
+        state.name = "张三";
+      });
+    },
+    changeMethod3(): any {
+      this.$patch((state) => {
+        state.name = "333";
+        state.age = 29;
+      });
+    },
+  },
+  // same as computed 计算属性
+  getters: {
+    count: (state) => {
+      return ++state.age;
+    },
+  },
   state: () => {
-    return {};
+    return {
+      name: "zwt",
+      age: 12,
+    };
   },
 });
